b0ed19de455d916337d320e857affbae8534c6fb
justiyz <inyange@yahoo.com>
2021-05-01 21:43:29 +0100

    Chapter5

--- src/main/java/com/servbytefooddelivery/other_exercises/thinking_in_java/chapter5/Chapter5Exercise1.java
+++ src/main/java/com/servbytefooddelivery/other_exercises/thinking_in_java/chapter5/Chapter5Exercise1.java
@@ -1,4 +1,15 @@
 package com.servbytefooddelivery.other_exercises.thinking_in_java.chapter5;
 
+// Create a class containing an uninitialized String reference. Demonstrate that // this reference is initialized by Java to null.
+
+class Tester {
+    String s;
+}
+
 public class Chapter5Exercise1 {
+
+    public static void main(String[] args) {
+        Tester t = new Tester();
+        System.out.println(t.s);
+    }
 }
