c608f3fbf2e8c25a4068840d3c6c780ad13f7677
justiyz <inyange@yahoo.com>
2021-05-01 21:52:26 +0100

    Chapter5

--- src/main/java/com/servbytefooddelivery/other_exercises/thinking_in_java/chapter5/Chapter5Exercise7.java
+++ src/main/java/com/servbytefooddelivery/other_exercises/thinking_in_java/chapter5/Chapter5Exercise7.java
@@ -1,4 +1,17 @@
 package com.servbytefooddelivery.other_exercises.thinking_in_java.chapter5;
 
+/* Create a class without a constructor, and then create an object of that
+ * class in main() to verify that the default constructor is automatically
+ * synthesized.
+ */
+
+class Aso {
+    void bark() {System.out.println("woof");}
+}
+
 public class Chapter5Exercise7 {
+    public static void main(String[] args) {
+        Aso a = new Aso();
+        a.bark();
+    }
 }
