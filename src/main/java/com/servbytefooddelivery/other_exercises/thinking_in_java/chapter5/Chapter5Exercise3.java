ba0797eecca5401632cba23eb463f68e58684359
justiyz <inyange@yahoo.com>
2021-05-01 21:46:10 +0100

    Chapter5

---
+++ src/main/java/com/servbytefooddelivery/other_exercises/thinking_in_java/chapter5/Chapter5Exercise3.java
@@ -0,0 +1,16 @@
+package com.servbytefooddelivery.other_exercises.thinking_in_java.chapter5;
+
+/* Create a class with a default constructor (one that takes no arguments) that
+ * prints a message. Create an object of this class.
+ */
+
+class Kabayo {
+    Kabayo() {
+        System.out.println("isa lang kabayo");
+    }
+}
+public class Chapter5Exercise3 {
+    public static void main(String[] args) {
+        Kabayo k = new Kabayo();
+    }
+}
