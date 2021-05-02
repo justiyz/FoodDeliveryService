4335524004840884f70724fcdf5659f840b6a6bf
justiyz <inyange@yahoo.com>
2021-05-02 12:32:45 +0100

    Chapter5

--- src/main/java/com/servbytefooddelivery/other_exercises/thinking_in_java/chapter5/Chapter5Exercise15.java
+++ src/main/java/com/servbytefooddelivery/other_exercises/thinking_in_java/chapter5/Chapter5Exercise15.java
@@ -1,4 +1,19 @@
 package com.servbytefooddelivery.other_exercises.thinking_in_java.chapter5;
 
+// Create a class wih a String that is initialized using instance initialization
+
+class Tester1 {
+    String s;
+    {
+        s = "Initializing string in Tester1";
+        System.out.println(s);
+    }
+    Tester1() {
+        System.out.println("Tester1()");
+    }
+}
 public class Chapter5Exercise15 {
+    public static void main(String[] args) {
+        new Tester1();
+    }
 }
