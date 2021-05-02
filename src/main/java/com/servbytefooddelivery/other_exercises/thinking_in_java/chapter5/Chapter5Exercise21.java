76420cd216ff4753f694865217509addbd57b60e
justiyz <inyange@yahoo.com>
2021-05-02 12:39:30 +0100

    Chapter5

--- src/main/java/com/servbytefooddelivery/other_exercises/thinking_in_java/chapter5/Chapter5Exercise21.java
+++ src/main/java/com/servbytefooddelivery/other_exercises/thinking_in_java/chapter5/Chapter5Exercise21.java
@@ -1,4 +1,14 @@
 package com.servbytefooddelivery.other_exercises.thinking_in_java.chapter5;
 
+/* Create an emun of the least-valuable six types of paper currency. Loop through
+ * the values() and print each value and its ordinal().
+ */
 public class Chapter5Exercise21 {
+    public enum Bills {
+        ONE, FIVE, TEN, TWENTY, FIFTY, HUNDRED
+    }
+    public static void main(String[] args) {
+        for(Bills b : Bills.values())
+            System.out.println(b + ", ordinal " + b.ordinal());
+    }
 }
