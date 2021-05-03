a5808c2e97b9a2ffa92291d18af53792c4ce4260
justiyz <inyange@yahoo.com>
2021-05-03 01:29:58 +0100

    Chapter7

---
+++ src/main/java/com/servbytefooddelivery/other_exercises/thinking_in_java/chapter7/Chapter7Exercise2.java
@@ -0,0 +1,24 @@
+package com.servbytefooddelivery.other_exercises.thinking_in_java.chapter7;
+
+/* Inherit a new class from class Detergent. Override scrub() and add a new method
+ * called sterilize().
+ */
+
+public class Chapter7Exercise2 extends Chapter7Exercise1{
+//    public void scrub() { append(" Sterilizer.scrub()"); }
+//    public void sterilize() { append(" sterilize()"); }
+//    public static void main(String[] args) {
+//        Chapter7Exercise2 x = new Chapter7Exercise2();
+//        x.dilute();
+//        x.apply();
+//        x.scrub();
+//        x.foam();
+//        x.sterilize();
+//        println(x);
+//        println("Testing base class:");
+//        Detergent.main(args);
+//    }
+
+
+    ////TODO: yet to be completed
+}
---
+++ src/main/java/com/servbytefooddelivery/other_exercises/thinking_in_java/chapter7/Chapter7Exercise3.java
@@ -0,0 +1,4 @@
+package com.servbytefooddelivery.other_exercises.thinking_in_java.chapter7;
+
+public class Chapter7Exercise3 {
+}
