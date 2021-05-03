f9d03ddbdadf8ce6c161d2967c3af1ba7733ba62
justiyz <inyange@yahoo.com>
2021-05-03 01:08:20 +0100

    Chapter6

---
+++ src/main/java/com/servbytefooddelivery/other_exercises/thinking_in_java/chapter6/Chapter6Exercise1.java
@@ -0,0 +1,19 @@
+package com.servbytefooddelivery.other_exercises.thinking_in_java.chapter6;
+
+// Create a class in a package. Create an instance of your class outside of that package.
+
+/* In another directory:
+ * // access/mypackage/MyPackagedClass.java
+ *
+ * package access.mypackage;
+ *
+ * public class MyPackagedClass {
+ *	public MyPackagedClass() {System.out.println("MyPackagedClass()");}
+ * }
+ */
+
+public class Chapter6Exercise1 {
+    public static void main(String[] args) {
+//        access.mypackage.MyPackagedClass m = new access.mypackage.MyPackagedClass();
+    }
+}
---
+++ src/main/java/com/servbytefooddelivery/other_exercises/thinking_in_java/chapter6/Chapter6Exercise5.java
@@ -0,0 +1,4 @@
+package com.servbytefooddelivery.other_exercises.thinking_in_java.chapter6;
+
+public class Chapter6Exercise5 {
+}
