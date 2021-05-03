f57580bf9bc80172ab7cfbf1a0d95454e6283943
justiyz <inyange@yahoo.com>
2021-05-03 01:31:48 +0100

    Chapter7

--- src/main/java/com/servbytefooddelivery/other_exercises/thinking_in_java/chapter7/Chapter7Exercise3.java
+++ src/main/java/com/servbytefooddelivery/other_exercises/thinking_in_java/chapter7/Chapter7Exercise3.java
@@ -1,4 +1,21 @@
 package com.servbytefooddelivery.other_exercises.thinking_in_java.chapter7;
 
+/* Prove the previous sentence. (Even if you don't create a constructor for
+ * Cartoon(), the compiler will synthesize a default constructor for you that
+ * calls the base-class constructor.
+ */
+
+class Art {
+    Art() {
+        System.out.println("Art constructor"); }
+}
+
+class Drawing extends Art {
+    Drawing() {
+        System.out.println("Drawing constructor"); }
+}
 public class Chapter7Exercise3 {
+    public static void main(String[] args) {
+        Chapter7Exercise3 x = new Chapter7Exercise3();
+    }
 }
